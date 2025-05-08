6296070177389a1d3817daf5e87c4d5d1852a254
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2025-05-09 02:54:14 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("This is a even number")
+else:
+    print("This is a odd number")
+    
+'''
+Enter a number: 2
+This is a even number
+
+Enter a number: 3
+This is a odd number
+'''
