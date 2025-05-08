895cd21952e80cc6d67f3b43e66be0b3313cadbd
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2025-05-09 02:53:49 +0530

    Add files via upload

---
+++ work.py
@@ -0,0 +1,18 @@
+#Check if a number is positive or negative
+
+num = float(input("Enter a number: "))
+if num > 0:
+      print("POSITIVE NUMBER")
+elif num == 0:
+      print("ZERO")
+else:
+      print("NEGATIVE NUMBER")
+      
+'''
+Enter a number: 0
+ZERO
+Enter a number: 3
+POSITIVE NUMBER
+Enter a number: -1
+NEGATIVE NUMBER
+'''
